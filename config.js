5c520e3103e646b5150b2b7481f6e78720c6df29
Edro6728 <100054872+Edro6728@users.noreply.github.com>
2022-10-11 20:11:48 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,22 +5,11 @@ import fs from 'fs'
 import { generateWAMessageFromContent } from '@adiwajshing/baileys'
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['34642467703', '👑 Mystic - Collaborator 1 👑', true],
-  ['50499698072', '👑 Mystic - Collaborator 2 👑', true],
-  ['51940617554', '👑 Mystic - Collaborator 3 👑', true],
-  ['51996089079', '👑 Mystic - Collaborator 4 👑', true],
-  ['573225236629', '👑 Mystic - Collaborator 5 👑', true],
-  ['593991398786', '👑 Mystic - Collaborator 6 👑', true],
-  ['593959425714', '👑 Mystic - Collaborator 7 👑', true],
-  ['593968585283'],
-  ['5219992095479'],
-  ['5219991402134'],
-  ['5219993404349'],
-  ['5219996125657']
+  ['3547911191', '👑 Mystic - Creador 👑', true],
+  ['33987777770']
 ] 
-global.suittag = ['5219993404349'] 
-global.prems = ['51995386439'] 
+global.suittag = ['3547911191'] 
+global.prems = ['3547911191'] 
 
 global.packname = '(☞ﾟ∀ﾟ)☞'
 global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
